a7ab027a525c4856a92704592a414f3188874053
Ling Qiu <lzq5034@psu.edu>
2024-09-17 22:35:41 -0400

    Refactor code and reorganize directories to improve efficiency and structure.

--- conversation_state_machine/base_state.py
+++
@@ -1,7 +0,0 @@
-class BaseState:
-    def get_speech(self, *args, **kwargs):
-        raise NotImplementedError("Subclasses should implement this method.")
-    def get_next_state(self, *args, **kwargs):
-        raise NotImplementedError("Subclasses should implement this method.")
-    def update_session_data(self, *args, **kwargs):
-        raise NotImplementedError("Subclasses should implement this method.")
--- conversation_state_machine/data/gui_data.py
+++
@@ -1,5 +0,0 @@
-
-import os
-
-GREETING_DOCUMENT = os.path.join(os.path.dirname(__file__), 'gui', 'greeting.json')
-#GREETING_DOCUMENT = ("./data/gui/greeting.json")
